26b88047ae55439a9af06ee676e2017a9f566169
Leandro Foly <l.foly@gsuite.iff.edu.br>
2026-09-10 14:36:35 -0300

    atualizacao

--- agenda_firebase/lib/main.dart
+++ agenda_firebase/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'tela_principal.dart';
 
+//comentário teste
+
+
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
